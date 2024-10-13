2324810e835f6d350ff3280cc7e0c320662f41db
JDamianDelgado <joakodelgado2013@gmail.com>
2024-10-13 00:21:23 -0300

    ejercicios c6 -finalizado

--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -3,6 +3,12 @@ function doWhile(num) {
   // Retorna el valor final.
   // PISTA: Utiliza el bucle do-while.
   // Tu código:
+  let aumenta = 0;
+  do {
+    num += 5;
+    aumenta++;
+  } while (aumenta < 8);
+  return num;
 }
 
 module.exports = doWhile;
