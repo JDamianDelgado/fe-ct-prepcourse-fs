07cb87e536485848cc64a990f93ee97c19974339
JDamianDelgado <joakodelgado2013@gmail.com>
2024-10-22 08:03:23 -0300

    finalizando c7 ejercicios

--- C7 Arrays/ejercicios/20.js
+++ C7 Arrays/ejercicios/20.js
@@ -2,6 +2,8 @@ function todosIguales(array) {
   // Si todos los elementos del arreglo son iguales, retornar true.
   // Caso contrario, retornar false.
   // Tu código:
+  let primerElemento = array[0];
+  return array.every((num) => num === primerElemento);
 }
 
 module.exports = todosIguales;
--- C7 Arrays/ejercicios/21.js
+++ C7 Arrays/ejercicios/21.js
@@ -3,6 +3,13 @@ function mesesDelAño(array) {
   // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
   // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
   // Tu código:
+  const mesesPedidos = ["Enero", "Marzo", "Noviembre"];
+  const mesesEncontrados = array.filter((mes) => mesesPedidos.includes(mes));
+  if (mesesEncontrados.length === mesesPedidos.length) {
+    return mesesEncontrados;
+  } else {
+    return "No se encontraron los meses pedidos";
+  }
 }
 
 module.exports = mesesDelAño;
