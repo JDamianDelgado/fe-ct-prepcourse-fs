4b1edf77bc74a10d00e13a0a17f0bb890840ef10
JDamianDelgado <joakodelgado2013@gmail.com>
2024-10-24 16:44:52 -0300

    finalizado ejercicios c8 objetos

--- C8 Objects + Callbacks/objects/ejercicios/20.js
+++ C8 Objects + Callbacks/objects/ejercicios/20.js
@@ -4,6 +4,12 @@ function sumarLikesDeUsuario(objetoUsuario) {
   // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
   // Debes sumar los likes de todos los post y retornar el resultado.
   // Tu código:
+
+  let resultado = 0;
+  for (let i = 0; i < objetoUsuario.post.length; i++) {
+    resultado += objetoUsuario.posts[i].likes;
+  }
+  return resultado;
 }
 
 module.exports = sumarLikesDeUsuario;
--- C8 Objects + Callbacks/objects/ejercicios/22.js
+++ C8 Objects + Callbacks/objects/ejercicios/22.js
@@ -1,10 +1,15 @@
 function esAnagrama(str1, str2) {
-   // La función recibe dos argumentos "str1" y "str2" que son strings.
-   // Determina si los dos strings son anagramas.
-   // Devuelve true de ser así, sino retorna false.
-   // IMPORTANTE: Un anagrama es una palabra que se forma
-   // con las mismas letras que otra, pero en orden diferente.
-   // Tu código:
+  // La función recibe dos argumentos "str1" y "str2" que son strings.
+  // Determina si los dos strings son anagramas.
+  // Devuelve true de ser así, sino retorna false.
+  // IMPORTANTE: Un anagrama es una palabra que se forma
+  // con las mismas letras que otra, pero en orden diferente.
+  // Tu código:
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  const str11 = str1.split("").sort().join("");
+  const str22 = str2.split("").sort().join("");
 }
-
+return str11 === str22;
 module.exports = esAnagrama;
